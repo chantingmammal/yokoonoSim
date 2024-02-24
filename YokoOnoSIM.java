2401ba68751474e7b7c542c947ed410ad07bb5d0
ezra <147104973+chantingmammal@users.noreply.github.com>
2024-02-24 09:15:59 -0500

    Update YokoOnoSIM.java

--- YokoOnoSIM.java
+++ YokoOnoSIM.java
@@ -31,25 +31,26 @@ public class YokoOnoSIM {
                 }
              }
         }
-        
+
+    /* This makes Yoko Ono Scream */
         public static void scream() {
             String[] screams = {"AAAAAAAAAAHHHH!", "YAAAAAAAAAAA!", "WOOOOOOOOOOOOO!"};
             Random random = new Random();
             String scream = screams[random.nextInt(screams.length)];
             System.out.println(scream);
         }
-        
+    /* This makes Yoko Ono Whisper */
         public static void whisper() {
             String[] whispers = {"I have a message for the world...", "Listen closely to what I have to say...", "Can you hear me? I have something important to share...", "John...", "Sean...", "Julian..."};
             Random random = new Random();
             String whisper = whispers[random.nextInt(whispers.length)];
             System.out.println(whisper);
             }
-            
+    /* This makes Yoko Ono do Art */
         public static void createArt() {
             //To be added
         }
-        
+    /* This makes Yoko break up her husband John's band "The Beatles" */
         public static void breakupBeatle() {
             System.out.println("Yoko Ono breaks up the Beatles, it's super effective!");
         }
